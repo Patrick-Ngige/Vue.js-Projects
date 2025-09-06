6b4ab7ac23e1949e7394824e0a348edb4f5ab962
Patrick-Ngige <wakemanjajr@gmail.com>
2025-09-06 22:37:03 +0300

    second project: creating an array of objects to hold the items from the list

--- simple-todo-list/src/App.vue
+++ simple-todo-list/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Mt TODO List</h1>
+    <h1>My TODO List</h1>
 
     <form action="">
       <input type="text" placeholder="Add a new task...">
@@ -12,4 +12,20 @@
     </ul>
 
   </div>
-</template>>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const newTaskText = ref('');
+
+// creating the object to hold the todo infos 
+const todos = ref([
+  { id: 1, text: 'Learning Vue Basics', completed: true },
+  { id: 2, text: 'Start off with simple projects', completed: false },
+])
+
+// the addTask method to add the item to the list and clear the input field 
+
+</script>
+
