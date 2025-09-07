dfd3c66d6cf2295db5b07d42e9a5f6be9233ff10
Patrick-Ngige <wakemanjajr@gmail.com>
2025-09-07 11:58:58 +0300

    third-project: creating the todoitem component

---
+++ advanced-todo-list/src/components/TodoItem.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+  todo: {
+    type: object,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <li :class="{ completed: todo.completed }">{{ todo.text }}</li>
+</template>
+
+<style scoped>
+li {
+  padding: 10px;
+  border-bottom: 1px solid #eee;
+  cursor: pointer;
+  transition: all 0.2s;
+}
+</style>
