658718137e530e17746138724e7cbdc911788c54
Patrick-Ngige <wakemanjajr@gmail.com>
2025-09-07 16:38:05 +0300

    setting up the app.vue template structure

--- 5. a-real-time-clock/src/App.vue
+++ 5. a-real-time-clock/src/App.vue
@@ -1,7 +1,26 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import Clock from './components/Clock.vue'
+
+//a ref to control the visibility of the clock
+const showClock = ref(true)
+
+function toggleClock() {
+  showClock.value = !showClock.value
+}
+</script>
 
 <template>
-  <h1>You did it!</h1>
+  <main class="app-container">
+    <h1>Lifecycle Hooks Demo</h1>
+    <button @click="toggleClock">
+      {{ showClock ? 'Hide Clock' : 'Show Clock' }}
+    </button>
+
+    <hr />
+
+    <Clock v-if="showClock" />
+  </main>
 </template>
 
 <style scoped></style>
