b3a827cf99f501898100aef476e4a4aaa4a38f9d
Patrick-Ngige <wakemanjajr@gmail.com>
2025-09-08 22:30:00 +0300

    setting up the Index.js

--- 8.a-settings-form/src/router/index.js
+++ 8.a-settings-form/src/router/index.js
@@ -1,8 +1,35 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/users',
+      name: 'users',
+      component: () => import('../views/UsersView.vue'),
+    },
+    {
+      path: '/users/:id',
+      name: 'usersDetail',
+      component: () => import('../views/UserDetailView.vue'),
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginView.vue'),
+    },
+  ],
 })
 
 export default router
