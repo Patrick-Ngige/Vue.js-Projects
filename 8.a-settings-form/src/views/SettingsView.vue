9741ab5c0b41ff18e04798f772668f5bb66ba744
Patrick-Ngige <wakemanjajr@gmail.com>
2025-09-08 22:33:56 +0300

    setting up the SettingsView.vue scripts

---
+++ 8.a-settings-form/src/views/SettingsView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { language } from '@vue/eslint-config-prettier/skip-formatting'
+import { ref } from 'vue'
+
+//reactive object holding all form data
+const settings = ref({
+  enableNotifications: true,
+  theme: 'light',
+  language: 'en',
+})
+</script>
