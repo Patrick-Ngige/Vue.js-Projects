85c622fbbe5808573f303352a1041eef57fade86
Patrick-Ngige <wakemanjajr@gmail.com>
2025-09-12 22:34:14 +0300

    setting up authStore.js

---
+++ 9.shared-user-store-with-pinia/src/stores/authStore.js
@@ -0,0 +1,28 @@
+import { defineStore } from 'pinia'
+
+//defineStore('storeName', { ... })
+export const useAuthStore = defineStore('auth', {
+  // State: the core data of the store
+  state: () => ({
+    isLoggedIn: false,
+    username: null,
+  }),
+
+  // Getters: computed properties for the store
+
+  // Actions: methods that modify the state
+  actions: {
+    login(name) {
+      //setting the state
+      this.isLoggedIn = true
+      this.username = name
+      console.log(`User '${this.username}' logged in.`)
+    },
+
+    logout() {
+      this.isLoggedIn = false
+      this.username = null
+      console.log('User logged out.')
+    },
+  },
+})
