7ece7bbfafbfe922691014cfb3f864c28c719d48
Patrick-Ngige <wakemanjajr@gmail.com>
2025-09-12 22:47:08 +0300

    setting up LoginView.vue template

--- 9.shared-user-store-with-pinia/src/views/LoginView.vue
+++ 9.shared-user-store-with-pinia/src/views/LoginView.vue
@@ -25,3 +25,16 @@ function performLogin() {
   }, 1000)
 }
 </script>
+
+<template>
+  <div class="page-content">
+    <h1>Login Page</h1>
+    <p>Enter a username and click login.</p>
+
+    <!-- A simple text input for the username -->
+    <input type="text" v-model="tempUsername" placeholder="Enter username" />
+    <button @click="performLogin" :disabled="isLoading">
+      {{ isLoading ? 'Logging in...' : 'Login' }}
+    </button>
+  </div>
+</template>
