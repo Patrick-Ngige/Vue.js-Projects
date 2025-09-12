55ca6711a7d2afecb7b6c837b60f8a4cee8c493b
Patrick-Ngige <wakemanjajr@gmail.com>
2025-09-12 22:43:22 +0300

    setting up LoginView.vue scripts

---
+++ 9.shared-user-store-with-pinia/src/views/LoginView.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { useAuthStore } from '@/stores/authStore'
+
+const router = useRouter()
+
+//an instance of the auth store
+const authStore = useAuthStore()
+
+const isLoading = ref(false)
+const tempUsername = ref('TestUser') //temporary username for the input
+
+function performLogin() {
+  isLosding.value = true
+
+  setTimeout(() => {
+    //calling login action from the store
+    authStore.login(tempUsername.value)
+
+    //redirecting when approved
+    router.push({ name: 'users' })
+
+    isLoading.value = false
+  }, 1000)
+}
+</script>
