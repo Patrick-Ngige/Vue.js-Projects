4b7a21ef5e363e373c6633e981613c23b91fbb4d
Patrick-Ngige <wakemanjajr@gmail.com>
2025-09-13 20:47:53 +0300

    finishing up the project

--- 12.handling-404-not-found-pages/src/views/NotFoundView.vue
+++ 12.handling-404-not-found-pages/src/views/NotFoundView.vue
@@ -3,7 +3,7 @@
     <h1>404 - Page Not Found!</h1>
     <p>Sorry, the page you are looking for does not exist.</p>
     <p>You can go back to the home page by clicking the button below.</p>
-    <RouterLink to="/" class="home-button">Go to Home.</RouterLink>
+    <RouterLink to="/login" class="home-button">Go to Home.</RouterLink>
   </div>
 </template>
 
