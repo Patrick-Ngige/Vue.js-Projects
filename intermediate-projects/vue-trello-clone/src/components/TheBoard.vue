19553d305987784f0e3f86629f6e1f0934ac1f27
Patrick-Ngige <wakemanjajr@gmail.com>
2025-09-14 12:17:59 +0300

    setting up TheBoard.vue parent component

---
+++ intermediate-projects/vue-trello-clone/src/components/TheBoard.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { useBoardStore } from '@/stores/boardStore'
+import BoardColumn from './BoardColumn.vue'
+
+//store instance
+const boardStore = useBoardStore()
+</script>
+
+<template>
+  <div class="board">
+    <!-- //looping through the columns in the store -->
+    <BoardColumn v-for="column in boardStore.getColumns" :key="column.id" :column="column" />
+  </div>
+</template>
