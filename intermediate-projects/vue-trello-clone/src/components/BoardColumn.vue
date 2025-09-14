1ce0e4cade143c40432c44112dbd6a227fefc5aa
Patrick-Ngige <wakemanjajr@gmail.com>
2025-09-14 12:07:42 +0300

    setting up the BoardColumn.vue component

---
+++ intermediate-projects/vue-trello-clone/src/components/BoardColumn.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+  columns: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="column">
+    <h2 class="column-title">{{ columns.title }}</h2>
+    <div class="card-list">
+      <div v-for="card in columns.cards" :key="card.id" class="card">
+        {{ card.content }}
+      </div>
+    </div>
+  </div>
+</template>
