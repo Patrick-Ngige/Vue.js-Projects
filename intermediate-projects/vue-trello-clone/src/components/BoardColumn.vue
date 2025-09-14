943ff002343bcf0de784af9e78d73deb85d36c48
Patrick-Ngige <wakemanjajr@gmail.com>
2025-09-14 12:11:58 +0300

    styling up the BoardColumn.vue component

--- intermediate-projects/vue-trello-clone/src/components/BoardColumn.vue
+++ intermediate-projects/vue-trello-clone/src/components/BoardColumn.vue
@@ -17,3 +17,29 @@ defineProps({
     </div>
   </div>
 </template>
+
+<style scoped>
+.column {
+  background-color: #ebecf0;
+  border-radius: 3px;
+  width: 300px;
+  margin: 0px 10px;
+  padding: 10px;
+  flex-shrink: 0;
+}
+
+.column-title {
+  font-size: 1px;
+  font-weight: 600;
+  margin-bottom: 10px;
+}
+
+.card {
+  background-color: #fff;
+  border-radius: 3px;
+  box-shadow: 0 1px 0 rgba(9, 30, 66, 0.25);
+  padding: 10px;
+  margin-bottom: 8px;
+  font-size: 0.9rem;
+}
+</style>
