4ecafc6673df1fe02f5fc0f583e0ef0635cad774
Patrick-Ngige <wakemanjajr@gmail.com>
2025-09-20 13:09:49 +0300

    new column styling setup

--- intermediate-projects/vue-trello-clone/src/components/NewColumnForm.vue
+++ intermediate-projects/vue-trello-clone/src/components/NewColumnForm.vue
@@ -23,3 +23,33 @@ function handleAddColumn() {
     <button type="submit" class="add-column-btn">Add Column</button>
   </form>
 </template>
+
+<style scoped>
+.new-column-form {
+  flex-shrink: 0;
+  width: 300px;
+  margin: 0px 10px;
+}
+.new-column-input {
+  background-color: hsla(0, 0, 100%, 0.24);
+  border: none;
+  border-radius: 3px;
+  color: white;
+  padding: 10px;
+  width: 100px;
+  box-sizing: border-box;
+  font-size: 1rem;
+}
+.new-column-input::placeholder {
+  color: hsla(0, 0, 100%, 0.7);
+}
+.add-column-btn {
+  background-color: #5aac44;
+  color: white;
+  border: none;
+  padding: 8px 12px;
+  border-radius: 3px;
+  cursor: pointer;
+  margin-top: 8px;
+}
+</style>
