0ba6b82048c484fec378c42808e5b15bc47d049c
Patrick-Ngige <wakemanjajr@gmail.com>
2025-09-20 13:03:26 +0300

    new column template and script setup

---
+++ intermediate-projects/vue-trello-clone/src/components/NewColumnForm.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue'
+import { useBoardStore } from '@/stores/boardStore'
+
+const boardStore = useBoardStore()
+const newColumnTitle = ref('')
+
+function handleAddColumn() {
+  if (newColumnTitle.value.trim() === '') return
+  boardStore.addColumn(newColumnTitle.value)
+  newColumnTitle.value = ''
+}
+</script>
+
+<template>
+  <form @submit.prevent="handleAddColumn" class="new-column-form">
+    <input
+      type="text"
+      v-model="newColumnTitle"
+      placeholder="+ Add another column"
+      class="new-column-input"
+    />
+    <button type="submit" class="add-column-btn">Add Column</button>
+  </form>
+</template>
