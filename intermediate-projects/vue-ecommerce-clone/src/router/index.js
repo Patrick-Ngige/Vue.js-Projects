8e5984433f28861a6a33ee72b1f13775cd0e6664
Patrick-Ngige <wakemanjajr@gmail.com>
2025-09-21 23:47:13 +0300

    updating the index.js with route

--- intermediate-projects/vue-ecommerce-clone/src/router/index.js
+++ intermediate-projects/vue-ecommerce-clone/src/router/index.js
@@ -1,8 +1,16 @@
+// src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router'
+import ProductView from '../views/ProductView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'product',
+      component: ProductView,
+    },
+  ],
 })
 
 export default router
