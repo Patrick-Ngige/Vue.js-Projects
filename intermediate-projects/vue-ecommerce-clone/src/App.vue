6782fbfb6309b72191de697048894daf734e829b
Patrick-Ngige <wakemanjajr@gmail.com>
2025-09-23 21:37:43 +0300

    correcting the App.vue structure

--- intermediate-projects/vue-ecommerce-clone/src/App.vue
+++ intermediate-projects/vue-ecommerce-clone/src/App.vue
@@ -1,12 +1,18 @@
 // src/App.vue
+
 <script setup>
 import { RouterView } from 'vue-router'
-import AppHeader from './components/AppHeader.vue'
+import AppHeader from './components/AppHeader.vue' // 1. Import the header component
 </script>
 
 <template>
   <div>
+    <!-- 2. Add the header component here -->
     <AppHeader />
-    <RouterView />
+
+    <!-- The RouterView will render the current page (ProductView) below the header -->
+    <main>
+      <RouterView />
+    </main>
   </div>
 </template>
